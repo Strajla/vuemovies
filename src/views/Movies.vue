dbec217721e662a97b08fb2ef9b8a34dbfc331ef
Strahinja <skansi97@gmail.com>
2020-10-16 15:49:06 +0200

    First task

---
+++ src/views/Movies.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <AppMovies/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppMovies from '@/components/AppMovies.vue'
+export default {
+  name: 'app-movies',
+  components: {
+    AppMovies
+  }
+}
+</script>
